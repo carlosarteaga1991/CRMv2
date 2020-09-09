5597335e644082630d7aa795a290be8646d920f4
carlosarteaga <carteaga338@gmail.com>
2020-09-09 05:27:50 -0600

    uso de ajax principal

--- app/cobros/urls.py
+++ app/cobros/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('departamento/',listview_departamento.as_view(), name='listar_departamento'),
     path('departamento/crear/',createview_departamento.as_view(), name='crear_departamento'),
     path('departamento/actualizar/<int:pk>/',updateview_departamento.as_view(), name='actualizar_departamento'),
+    path('departamento/borrar/<int:pk>/',deleteview_departamento.as_view(), name='borrar_departamento'),
 ]
--- app/cobros/views/Departamento/views.py
+++ app/cobros/views/Departamento/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse, JsonResponse,HttpResponseRedirect
 from app.cobros.models import Departamentos,Clientes
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt,csrf_protect
 from app.cobros.forms import form_departamento
@@ -36,6 +36,38 @@ def listar_departamento(request):
 class listview_departamento(ListView):
     model = Departamentos
     template_name = 'Departamento/listar.html'
+
+    #esto para filtrar todos los q estén activos
+    def get_queryset(self):
+        return self.model.objects.filter(estado=1)
+
+    # sobre escribiendo el método POST
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request,*args,**kwargs):
+        return super().dispatch(request,*args,**kwargs)
+
+    def post(self,request,*args,**kwargs):
+        data = {}
+        # para controlar en caso q despliegue error
+        try:
+            # #1: se comenta para hacer la cargada en la taba usandoi AJAX
+            # #1 data = Departamentos.objects.get(pk=request.POST['id_departamento']).toJSON() #sino existe error llamará al método tiJSON definido en models.py en departamentos
+            action = request.POST['action']
+            if action == 'searchdata':
+                data = []
+                for i in Departamentos.objects.filter(estado=1): # si deseamos todos colocamos .all()
+                    data.append(i.toJSON())
+            else:
+                data['error']='Ha ocurrido un error al cargar con AJAX'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data, safe=False)
+        # #2: se comenta ya que como se envía una coleccion de diccionarios y vienen serializados se coloca el atributo, safe=False.
+        # #2return JsonResponse(data)
+        
+
+
+    
     
     #para enviar un diccionario en la clase hay que sobre escribir el método get_context
     def get_context_data(self, **kwargs):
@@ -47,21 +79,8 @@ class listview_departamento(ListView):
         context['create_url'] = reverse_lazy('crm:crear_departamento')
         return context
 
-    # sobre escribiendo el método POST
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request,*args,**kwargs):
-        return super().dispatch(request,*args,**kwargs)
 
-    def post(self,request,*args,**kwargs):
-        data = {}
-        # para controlar en caso q despliegue error
-        try:
-            data = Departamentos.objects.get(pk=request.POST['id_departamento']).toJSON() #sino existe error llamará al método tiJSON definido en models.py en departamentos
-        except Exception as e:
-            data['error'] = str(e)
-        
-        return JsonResponse(data)
-
+    
 class createview_departamento(CreateView):
     model = Departamentos
     form_class = form_departamento # llamamos al formulario creado en forms.py y hay q importarlo
@@ -103,3 +122,34 @@ class updateview_departamento(UpdateView):
         context['btn_cancelar'] = reverse_lazy('crm:listar_departamento')
         context['titulo_lista'] = 'Editar departamento'
         return context
+
+class deleteview_departamento(DeleteView):
+    model = Departamentos
+    template_name = 'Departamento/borrar.html'
+    success_url = reverse_lazy('crm:listar_departamento')
+
+    #@method_decorator(csrf_exempt)
+    def dispatch(self, request,*args,**kwargs):
+        self.object = self.get_object()
+        return super().dispatch(request,*args,**kwargs)
+
+    def post(self, request,*args,**kwargs):
+        data = {}
+        try:
+            #self.object.delete()
+            registro = self.get_object()
+            registro.estado = 2
+            registro.save()
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['nombre'] = 'carlos arteaga'
+        context['plantilla'] = 'Eliminar'
+        context['btn_cancelar'] = reverse_lazy('crm:listar_departamento')
+        context['list_url'] = reverse_lazy('crm:listar_departamento')
+        context['titulo_lista'] = 'Eliminar departamento'
+        return context
+
